29896d24bc89f3ab3d08958a23ab2422a5c4a851
arpitajangir1111 <arpita8857@gmail.com>
2023-09-08 10:18:27 -0700

    first

---
+++ counter/src/counter/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {decrementCounter ,incrementCounter} from './slice'
+import { useDispatch,useSelector } from 'react-redux'
+
+
+function App() {
+    
+    const dispatch =useDispatch()
+    const init = useSelector((state)=>{return state.counterRed})
+
+  return (
+    <div>
+        <h2>Counter using Redux</h2>
+        <button onClick={()=> dispatch(incrementCounter())}>Increment</button>
+        <p>{init.counter}</p>
+        <button onClick={()=>dispatch(decrementCounter())}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
